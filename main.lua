0c5cf190ce86c387539ad9db4a663032d2351706
joelleishimwe <joelle.ishimwe@ocsbstudent.ca>
2018-03-01 10:46:46 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,53 @@
+-- Title: MovingImages
+-- Name: Joelle Ishimwe
+-- Course: ICS2O
+-- This program displays a beetleship moving across the screen, aswell 
+-- as another object moving in different direction
+----------------------------------------------------------------------------------
+
+print ("***Testing beetleship!")
+----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
